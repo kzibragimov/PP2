1c4b889d89867460b11a464fcef4a9c01bf92e49
kzibragimov <aibar.111.00@gmail.com>
2019-02-14 13:44:16 +0600

    week1,2

---
+++ week2/Task1/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader s = new StreamReader("palin.txt");
+            string n = s.ReadToEnd();
+            char[] m = n.ToCharArray();
+            Array.Reverse(m);
+            string b = new string(m);
+            if (n.Equals(b))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+        }
+    }
+}
---
+++ week2/Task2/Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        public static bool IsPrime(int x)
+        {
+            if (x == 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+        static void Main(string[] args)
+        {
+            StreamReader s = new StreamReader("prime.txt");
+            string a = s.ReadToEnd();
+            string[] arr = a.Split();
+            s.Close();
+            StreamWriter sw = new StreamWriter("output.txt");
+            for (int i = 0; i < arr.Count(); i++)
+            {
+                if (IsPrime(int.Parse(arr[i])) == true)
+                {
+                    sw.Write(arr[i] + " ");
+                }
+            }
+            sw.Close();
+        }
+    }
+}
+
